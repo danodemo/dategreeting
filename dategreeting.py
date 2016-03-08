997940e6ea82905fa00183d6fe23e93f12233f8e
Daniel Nicodemo <dan.nicodemo@gmail.com>
2016-03-08 11:21:44 -0500

    added rawinput for name

--- dategreeting.py
+++ dategreeting.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-name = "Dan"
-now = datetime.now()
+name = raw_input()"Hello! What is your name?"
+now_local = datetime.now()
 
-print "Hello, %s, and welcome to the console!  The current date and time is %s. " % (name, now)
+print "Hello, %s, and welcome to the console!  The current date and time is %s. " % (name, now_local)
 
